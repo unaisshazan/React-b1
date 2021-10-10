598bd583542be3c46c9483cd495e149500ec9c18
unaisshazan <51183141+unaisshazan@users.noreply.github.com>
2021-10-10 21:07:32 +0500

    add

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class About extends Component{
-    render(){
-        return(<div>
-            <div className="container">
-            <h2 className="text-center">About Us</h2>
-            </div>
-            </div>)
-    }
-
-
+class About extends Component {
+	render() {
+		return (
+			<div>
+				<div className="container">
+					<h2 className="text-center">About Us</h2>
+					<p>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae
+						maiores sint expedita, sequi porro quidem. Velit iusto, voluptates
+						illo sint sapiente magni vel beatae. Distinctio expedita dolor sit
+						consequuntur ea.
+					</p>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default About;
--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./main.css";
 
-class Services extends Component{
-    render(){
-        return(<div>Services Page</div>)
-    }
-
-
+class Services extends Component {
+	render() {
+		return (
+			<div>
+				<section id="services">
+					<div className="container">
+						<div className="text-center">
+							<h2>Our Sevices</h2>
+                            <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam pariatur expedita corporis facere neque fugiat cupiditate quisquam modi ad sapiente, qui vitae voluptate id impedit, ut minima? Facilis, deleniti corrupti?</p>
+						</div>
+					</div>
+				</section>
+			</div>
+		);
+	}
 }
 
 export default Services;
