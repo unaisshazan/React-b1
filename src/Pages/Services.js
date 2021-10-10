0289da287d400bd4ae1804be641f8a28bc1cb58e
unaisshazan <51183141+unaisshazan@users.noreply.github.com>
2021-10-10 21:25:59 +0500

    add

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
 import "./main.css";
+import Service1 from '../images/services/services1.png'
+import Service2 from '../images/services/services2.png'
+import Service3 from '../images/services/services3.png'
+import Service4 from '../images/services/services4.png'
+import Service5 from '../images/services/services5.png'
+import Service6 from '../images/services/services6.png'
 
 class Services extends Component {
 	render() {
 		return (
 			<div>
-				<section id="services">
+				<section id="services" className="p-4">
 					<div className="container">
 						<div className="text-center">
 							<h2>Our Sevices</h2>
-                            <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam pariatur expedita corporis facere neque fugiat cupiditate quisquam modi ad sapiente, qui vitae voluptate id impedit, ut minima? Facilis, deleniti corrupti?</p>
+							<p className="lead">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+								pariatur expedita corporis facere neque fugiat cupiditate
+								quisquam modi ad sapiente, qui vitae voluptate id impedit, ut
+								minima? Facilis, deleniti corrupti?
+							</p>
+						</div>
+						<div className="row">
+							<div className="col-md-4">
+                                <img src={Service1} alt="" />
+                            </div>
 						</div>
 					</div>
 				</section>
