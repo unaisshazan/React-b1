6c0dc07ff30fbadd8166d80b14db222309436c31
unaisshazan <51183141+unaisshazan@users.noreply.github.com>
2021-10-27 21:51:36 +0500

    add

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import blog1 from '../images/blog/blog1.jpg'
-import blog2 from '../images/blog/blog2.jpg'
+import blog1 from "../images/blog/blog1.jpg";
+import blog2 from "../images/blog/blog2.jpg";
+import "./main.css";
 class Blog extends Component {
 	render() {
 		return (
@@ -11,18 +12,44 @@ class Blog extends Component {
 							<h2>Blogs</h2>
 							<p className="lead">
 								Lorem ipsum dolor sit amet consectetur adipisicing elit.
-	
 							</p>
-
 						</div>
-                        <div className="row"> 
-                        <div className="col-md-6">
-                       <img src={blog1} alt="" />
-                        </div>
-                        <div className="col-md-6">
-                        <img src={blog2} alt="" />
-                            </div>
-                         </div>
+						<div className="row">
+							<div className="col-md-6 blog-content">
+								<img src={blog1} alt=""  className="img-blog"/>
+								<h3>
+								
+									<a href="">
+										Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									</a>
+								</h3>
+								<p>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Illum, repellat fuga! Consequuntur repellat sed facere autem
+									non eius aliquid voluptatem, repudiandae molestiae, voluptatum
+									dignissimos aperiam blanditiis consectetur perferendis nisi
+									libero.
+								</p>
+								<button className="btn btn-primary"> Read More ....</button>
+							</div>
+							<div className="col-md-6">
+							<img src={blog2} alt=""  className="img-blog"/>
+								<h3>
+								
+									<a href="">
+										Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									</a>
+								</h3>
+								<p>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Illum, repellat fuga! Consequuntur repellat sed facere autem
+									non eius aliquid voluptatem, repudiandae molestiae, voluptatum
+									dignissimos aperiam blanditiis consectetur perferendis nisi
+									libero.
+								</p>
+								<button className="btn btn-primary"> Read More ....</button>
+							</div>
+						</div>
 					</div>
 				</section>
 			</div>
