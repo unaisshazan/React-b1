595e27d24672710f58638671a102f01b5cb40793
unaisshazan <51183141+unaisshazan@users.noreply.github.com>
2021-10-30 20:46:30 +0500

    Update Contact.js

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 
 class Contact extends Component{
     render(){
-        return(<div>Contact Page</div>)
+        return(<div>
+
+            <section  className="pt-4">
+                <div className="container">
+                    <div className="text-center">
+                        <h2> Drop Your Message</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+
+                    </div>
+                    <div> <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeTamYwEf9GrlA04lVQ-TJeHSJXtWcQUCnbY4Pj_JL57TpGbw/viewform?embedded=true" width="640" height="800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+</div>
+                </div>
+            </section>
+        </div>)
     }
 
 
