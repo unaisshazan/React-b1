f9551690941f255210aae3a5aab618b4f7469b9d
unaisshazan <51183141+unaisshazan@users.noreply.github.com>
2021-11-07 22:17:22 +0500

    Update Services.js

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./main.css";
 import Service1 from "../images/services/services1.png";
-import loader from '../images/loader.gif'
+import BlackLoader from '../images/loader.gif'
 import client from './Client'
 
 
@@ -25,47 +25,49 @@ class Services extends Component {
 	render() {
 		return (
 			<div>
-			<section id="services" className="p-4">
-				<div className="container">
-					<div className="text-center">
-						<h2>Our Services</h2>
-						<p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta in ipsum non facilisis.</p>
-					</div>
-					{this.state.servicepage.length === 0 ?
-						<div align="center"><img src={loader} alt="loader" /></div>
-
-						:
-						<div className="row">
-
-							{this.state.servicepage.map((item, index) => {
+				<section id="services" className="p-4">
+					<div className="container">
+						<div className="text-center">
+							<h2>Our Sevices</h2>
+							<p className="lead">
+								We are giving following
+							</p>
+						</div>
+						{this.state.servicepage.length === 0 ?
+					<div align="center" className="pt-5"> <img src={BlackLoader} alt="Loader" /> </div>
+					:
+					<div className="row">
+						{this.state.servicepage.map((item) => {
 								
-								return (
-									<div key={index} className="col-md-4">
-										<div className="media service-wrap">
-											<div>
-												<img className="pr-3" src={item.fields.serviceIcon.fields.file.url} alt="" />
-											</div>
-											<div className="media-body">
-												<h3 className="media-heading">{item.fields.serviceTitle}</h3>
-												<p>{item.fields.serviceDescription}</p>
-											</div>
+								return(
+									<div className="col-md-4">
+									<div className="media service-wrap">
+										<div>
+											<img className="pr-3" src={item.fields.serviceIcon.fields.file.url} alt="" />
+										</div>
+										<div className="media-body">
+											<h3 className="media-heading">{item.fields.serviceTitle}</h3>
+											<p>
+												{item.fields.serviceDescription}
+											</p>
 										</div>
 									</div>
+								</div>
 								)
 							}
-
-
-							)}
-
-
-
+			
+						
+                           
+						   )	}
+                          
+                
 
 						</div>
 					}
-
-				</div>
-			</section>
-		</div>
+						
+					</div>
+				</section>
+			</div>
 		);
 	}
 }
