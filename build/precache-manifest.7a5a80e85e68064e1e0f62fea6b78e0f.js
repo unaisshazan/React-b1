b060e819aba929c9e8865c488c9e412674dc51be
unaisshazan <51183141+unaisshazan@users.noreply.github.com>
2021-11-13 20:45:24 +0500

    add

---
+++ build/precache-manifest.7a5a80e85e68064e1e0f62fea6b78e0f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1a02c836a34ba0b9822",
+    "url": "/static/css/main.da1602db.chunk.css"
+  },
+  {
+    "revision": "a1a02c836a34ba0b9822",
+    "url": "/static/js/main.a1a02c83.chunk.js"
+  },
+  {
+    "revision": "0ccf8f11c3b8ea73c58e",
+    "url": "/static/js/1.0ccf8f11.chunk.js"
+  },
+  {
+    "revision": "49040564fd0d01396051",
+    "url": "/static/js/2.49040564.chunk.js"
+  },
+  {
+    "revision": "b317d3ca491a431219dc",
+    "url": "/static/js/runtime~main.b317d3ca.js"
+  },
+  {
+    "revision": "afa1e6360eb87c75dbc14b52aa4323eb",
+    "url": "/static/media/bg_services.afa1e636.jpg"
+  },
+  {
+    "revision": "49756efb5d61f03def5cbc1ae6f554ca",
+    "url": "/static/media/public1.49756efb.jpg"
+  },
+  {
+    "revision": "1b21572c0c56ef8a409826018f866914",
+    "url": "/static/media/public2.1b21572c.jpg"
+  },
+  {
+    "revision": "c6969165b945c8b278f08e7c3d39036f",
+    "url": "/static/media/public3.c6969165.jpg"
+  },
+  {
+    "revision": "90397e71f71cbfa8d00bb0dbae380df7",
+    "url": "/static/media/blog1.90397e71.jpg"
+  },
+  {
+    "revision": "219a80c8e8058f52e2726c2a0f79c201",
+    "url": "/static/media/blog2.219a80c8.jpg"
+  },
+  {
+    "revision": "9579942196c90d2456707679b587771e",
+    "url": "/index.html"
+  }
+];
