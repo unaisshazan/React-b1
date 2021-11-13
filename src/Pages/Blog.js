f017487bea510929127a19869e5470b5db137368
unaisshazan <51183141+unaisshazan@users.noreply.github.com>
2021-11-13 21:22:19 +0500

    Update Blog.js

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -15,7 +15,7 @@ class Blog extends Component {
 				order: "-sys.createdAt",
 			})
 			.then((entries) => {
-				this.setState({ blogpage: entries.items.length });
+				this.setState({ blogpage: entries.items });
 			});
 	}
 	render() {
@@ -35,16 +35,12 @@ class Blog extends Component {
 									<div key={index} className="col-md-6 blog-content">
 										<img src={item.fields.blogThumbnail.fields.file.url} alt="" className="img-blog img-fluid" />
 										<h3>
-											<Link href=""> 
-												Lorem ipsum dolor sit amet consectetur adipisicing elit.
+											<Link to={`../Blogs/${item.fields.slug}`}> 
+											{item.fields.blogTitle}
 											</Link>
 										</h3>
 										<p>
-											Lorem ipsum dolor sit amet consectetur adipisicing elit.
-											Illum, repellat fuga! Consequuntur repellat sed facere
-											autem non eius aliquid voluptatem, repudiandae molestiae,
-											voluptatum dignissimos aperiam blanditiis consectetur
-											perferendis nisi libero.
+											{item.fields.blogDescription}
 										</p>
 										<button className="btn btn-primary"> Read More ....</button>
 									</div>
