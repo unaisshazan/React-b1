df5a2d4e40f036ca9163dedddb861dd46874e036
unaisshazan <51183141+unaisshazan@users.noreply.github.com>
2021-11-21 21:19:38 +0500

    Update SingleArticle.js

--- src/Pages/SingleArticle.js
+++ src/Pages/SingleArticle.js
@@ -11,30 +11,44 @@ class SingleArticle extends Component {
 		this.state = { article: [] };
 	}
 	componentDidMount() {
-        const slug = this.props.match.params.slug
-       
-    if (slug){
-        client
-        .getEntries({
-            content_type: "blog" ,"fields.slug":slug
-        })
-        .then((entries) => {
-            this.setState({article:entries.items[0] });
-        });
-}
+		const slug = this.props.match.params.slug;
 
-    }
-	
+		if (slug) {
+			client
+				.getEntries({
+					content_type: "blog",
+					"fields.slug": slug,
+				})
+				.then((entries) => {
+					this.setState({ article: entries.items[0] });
+				});
+		}
+	}
+	getParsedMarkdown(blogDescription) {
+		return {
+			__html: marked(blogDescription, { sanitize: true }),
+		};
+	}
+
 	render() {
 		return (
-			<div className="container">
-				<div className="row">
-					<div className="col-md-12">
-						<h2>This is blog Title</h2>
-						<p>This is blog desc</p>
-						<p> Back</p>
+			<div>
+				{this.state.article.length === 0 ? (
+					<div align="center" className="pt-5">
+						{" "}
+						<img src={BlackLoader} alt="Loader" />{" "}
 					</div>
-				</div>
+				) : (
+					<div className="container">
+						<div className="row">
+							<div className="col-md-12">
+								<h2>This is blog Title</h2>
+								<p>This is blog desc</p>
+								<p> Back</p>
+							</div>
+						</div>
+					</div>
+				)}
 			</div>
 		);
 	}
